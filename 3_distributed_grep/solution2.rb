8b93d1e8686e1b2974636db18496a69e55d36fb3
Andy Kent <andrew.d.kent@gmail.com>
2009-10-07 17:05:46 +0100

    grep examples

--- 3_distributed_grep/solution2.rb
+++ 3_distributed_grep/solution2.rb
@@ -1,12 +1,12 @@
 require "rubygems"
 require "mandy"
 
-Mandy.job "Grep" do
+Mandy.job "Super Grep" do
   map_tasks 5
   reduce_tasks 3
   
   map do |line|
-    emit(line, 1) if (line =~ /Alice/)
+    emit(line, 1) if (line =~ /#{parameter(:pattern)}/)
   end
   
   reduce do |line, matches|
